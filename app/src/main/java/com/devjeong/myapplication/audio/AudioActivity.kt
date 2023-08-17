581400437c0941906a84d0202ec515b686ab72db
JeongSG <jsk41755@naver.com>
2023-08-17 21:52:36 +0900

    #3.3 fragment_ranking.xml 제작

---
+++ app/src/main/java/com/devjeong/myapplication/audio/AudioActivity.kt
@@ -0,0 +1,12 @@
+package com.devjeong.myapplication.audio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devjeong.myapplication.R
+
+class AudioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+    }
+}
