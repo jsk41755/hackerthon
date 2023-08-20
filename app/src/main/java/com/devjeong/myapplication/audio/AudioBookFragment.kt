cc254bf772b77f4a77cf8e266dd1be8fc008be25
JeongSG <jsk41755@naver.com>
2023-08-20 15:16:05 +0900

    #8 ExoPlayer 활용 성공, position에 따른 text 컬러 변경 완료. 이제 자동 RecyclerView 내려가기 구현해 주어야 함.

--- app/src/main/java/com/devjeong/myapplication/audio/AudioBookFragment.kt
+++ app/src/main/java/com/devjeong/myapplication/audio/AudioBookFragment.kt
@@ -1,18 +1,23 @@
 package com.devjeong.myapplication.audio
 
-import android.os.Bundle
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devjeong.myapplication.R
 import com.devjeong.myapplication.UtilityBase
+import com.devjeong.myapplication.audio.model.fetchAudioUrl
 import com.devjeong.myapplication.databinding.FragmentAudioBookBinding
-import kotlinx.coroutines.flow.collect
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
 import kotlinx.coroutines.launch
 
 class AudioBookFragment
@@ -21,31 +26,101 @@ class AudioBookFragment
     private val viewModel: AudioBookViewModel by activityViewModels()
     private lateinit var adapter: AudioBookScriptAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var player: ExoPlayer
+    private var playbackPosition: Int = 0
 
-    }
+    private val speaker: String = "vyuna"
+    private var selectNum : Int = 2
+    private var autoPlayHandler: Handler? = null
+    private val autoPlayDelay: Long = 5000 // 5 seconds
+    private var isAutoPlaying: Boolean = false
+
 
     override fun FragmentAudioBookBinding.onCreateView(){
         binding.lifecycleOwner = viewLifecycleOwner
         binding.audioBookViewModel = viewModel
 
-        adapter = AudioBookScriptAdapter(emptyList())
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+
+        //adapter = AudioBookScriptAdapter(emptyList())
+        adapter = AudioBookScriptAdapter(emptyList()) { position ->
+            playbackPosition = position
+            fetchAudioAndPlay(playbackPosition)
+            startAutoPlay()
+        }
+
         binding.audioBookScriptRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.audioBookScriptRecyclerView.adapter = adapter
 
-        lifecycleScope.launch {
+        /*lifecycleScope.launch {
             viewModel.audioBookScript.collect { audioBookScriptList ->
                 val contents = audioBookScriptList.flatMap { it.contents }
                 adapter = AudioBookScriptAdapter(contents)
                 binding.audioBookScriptRecyclerView.adapter = adapter
                 adapter.notifyDataSetChanged()
             }
+        }*/
+
+        lifecycleScope.launch {
+            viewModel.audioBookScript.collect { audioBookScriptList ->
+                val contents = audioBookScriptList.flatMap { it.contents }
+                adapter.updateContents(contents) // Adapter의 updateContents 메서드 호출
+            }
         }
 
-        val bookId = 1
+        val bookId = 2
         viewModel.fetchBookScript(bookId)
-
     }
 
+    private fun fetchAudioAndPlay(position: Int) {
+        val content = adapter.getItem(position)
+
+        // Create a new background thread
+        val backgroundThread = HandlerThread("AudioFetchThread")
+        backgroundThread.start()
+
+        val backgroundHandler = Handler(backgroundThread.looper)
+
+        backgroundHandler.post {
+            val audioUrl = fetchAudioUrl(requireContext(), content, speaker)
+
+            // Use the main thread to play the audio
+            Handler(Looper.getMainLooper()).post {
+                if (audioUrl.isNotEmpty()) {
+                    val mediaItem = MediaItem.fromUri(audioUrl)
+                    player.setMediaItem(mediaItem)
+                    player.prepare()
+                    player.playWhenReady = true
+                    adapter.setPlayingPosition(position)
+                }
+            }
+        }
+    }
+
+    private fun startAutoPlay() {
+        isAutoPlaying = true
+        player.addListener(object : Player.Listener {
+            override fun onPlaybackStateChanged(state: Int) {
+                if (state == Player.STATE_ENDED) {
+                    if (isAutoPlaying && playbackPosition < adapter.itemCount - 1) {
+                        // Move to the next item and play
+                        playbackPosition++
+                        fetchAudioAndPlay(playbackPosition)
+                    } else {
+                        // Playback has finished, reset auto play
+                        isAutoPlaying = false
+                    }
+                }
+            }
+
+            // ... (다른 콜백 메서드들)
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        player.release()
+        autoPlayHandler?.removeCallbacksAndMessages(null)
+
+    }
 }
--- app/src/main/java/com/devjeong/myapplication/audio/AudioBookScriptAdapter.kt
+++ app/src/main/java/com/devjeong/myapplication/audio/AudioBookScriptAdapter.kt
@@ -3,16 +3,43 @@ package com.devjeong.myapplication.audio
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.devjeong.myapplication.R
 import com.devjeong.myapplication.databinding.AudioBookItemBinding
 
-class AudioBookScriptAdapter(private val contents: List<String>)
-    : RecyclerView.Adapter<AudioBookScriptAdapter.AudioBookViewHolder>() {
+class AudioBookScriptAdapter(
+    private var contents: List<String>,
+    private val playCallback: (Int) -> Unit
+) : RecyclerView.Adapter<AudioBookScriptAdapter.AudioBookViewHolder>() {
 
     override fun onBindViewHolder(holder: AudioBookViewHolder, position: Int) {
         val content = contents[position]
-        holder.bind(content)
+        holder.bind(content, position)
+        holder.itemView.setOnClickListener {
+            playCallback.invoke(position) // 클릭 이벤트가 발생할 때 playCallback 호출
+        }
     }
+
+    fun updateContents(newContents: List<String>) {
+        contents = newContents
+        notifyDataSetChanged()
+    }
+
+    private var playingPosition: Int = -1
+
+    fun setPlayingPosition(position: Int) {
+        val previousPosition = playingPosition
+        playingPosition = position
+        // Notify the adapter to update the views for the previous and current items
+        if (previousPosition != -1) {
+            notifyItemChanged(previousPosition)
+        }
+        if (playingPosition != -1) {
+            notifyItemChanged(playingPosition)
+        }
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AudioBookViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -24,12 +51,24 @@ class AudioBookScriptAdapter(private val contents: List<String>)
         return contents.size
     }
 
-    class AudioBookViewHolder(private val binding: AudioBookItemBinding) :
+    fun getItem(position: Int): String {
+        return contents[position]
+    }
+
+    inner class AudioBookViewHolder(private val binding: AudioBookItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(content: String) {
+        fun bind(content: String, position: Int) {
             binding.script = content
             binding.executePendingBindings()
+
+            binding.audioBookScript.setTextColor(
+                if (position == playingPosition) {
+                    ContextCompat.getColor(itemView.context, R.color.white)
+                } else {
+                    ContextCompat.getColor(itemView.context, R.color.black)
+                }
+            )
         }
     }
 }
